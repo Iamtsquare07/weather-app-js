c5cb7b92a5b3591924e47d47bf46602363120d06
Victor Tyavbee <68505976+Iamtsquare07@users.noreply.github.com>
2023-10-15 02:17:28 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 // Replace with your actual API key, you can get the API Key here: https://openweathermap.org/api
-const apiKey = "95a8aff3374ba1428ae34b7607c60b5a";
+const apiKey = "";
 const tempField = document.getElementById("temperature");
-const city = document.getElementById("city");
+const region = document.getElementById("region");
 const weatherInfo = document.getElementById("weather-info");
+const conditionField = document.querySelector(".right");
+const weatherContainer = document.querySelector(".weather");
+const cityInput = document.getElementById("city");
+let weatherCondition;
 
 async function getWeatherByCity() {
-  const cityInput = document.getElementById("city");
   const city = cityInput.value;
 
   if (!city) {
@@ -25,18 +28,22 @@ async function getWeatherByCity() {
       const cityName = data.name;
       const country = data.sys.country;
 
-      city.innerHTML = `<h2>Weather in ${cityName}, ${country}:</h2>`;
-      tempField.textContent = `${temperature}°C`;
-      const weatherText = ` ${description}`;
+      region.innerHTML = `<h4>Weather in ${cityName}, ${country}</h4>`;
+      tempField.textContent = `${temperature.toFixed(0)}°C`;
+      const weatherText = ` ${capitalizeFirstLetter(description)}`;
       weatherInfo.textContent = weatherText;
 
       getWeatherForecast(cityName);
+
+      weatherCondition = getWeatherCondition(temperature);
+      getWeatherIcon(weatherCondition);
     } else {
       weatherInfo.textContent = "City not found. Please check the city name.";
     }
   } catch (error) {
     console.error("Error:", error);
-    weatherInfo.textContent = "An error occurred while fetching weather data.";
+    console.log("An error occurred while fetching weather data.");
+    alert("An error occurred while fetching weather data.");
   }
 }
 
@@ -58,19 +65,22 @@ async function getWeatherByLocation() {
           const cityName = data.name;
           const country = data.sys.country;
 
-          city.textContent = `Weather in ${cityName}, ${country}:`;
-          tempField.textContent = `${temperature}°C`;
-          const weatherText = ` ${description}`;
+          region.innerHTML = `<h4>Weather in ${cityName}, ${country}</h4>`;
+          tempField.textContent = `${temperature.toFixed(0)}°C`;
+          const weatherText = ` ${capitalizeFirstLetter(description)}`;
           weatherInfo.textContent = weatherText;
           getWeatherForecast(cityName);
+
+          weatherCondition = getWeatherCondition(temperature);
+          getWeatherIcon(weatherCondition);
         } else {
           weatherInfo.textContent =
             "Error fetching weather data for your location. Please try again.";
         }
       } catch (error) {
         console.error("Error:", error);
-        weatherInfo.textContent =
-          "An error occurred while fetching weather data.";
+        console.log("An error occurred while fetching weather data.");
+        alert("An error occurred while fetching weather data.");
       }
     });
   } else {
@@ -79,6 +89,9 @@ async function getWeatherByLocation() {
 }
 
 document.addEventListener("DOMContentLoaded", getWeatherByLocation);
+cityInput.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") getWeatherByCity();
+});
 
 async function getWeatherForecast(city) {
   const forecastInfo = document.getElementById("weather-forecast");
@@ -95,7 +108,7 @@ async function getWeatherForecast(city) {
     const data = await response.json();
 
     if (response.status === 200) {
-      forecastInfo.innerHTML = "<h2>Daily Weather Forecast:</h2>";
+      forecastInfo.innerHTML = "<h2>Weather Forecast:</h2>";
       const forecasts = data.list;
       const dailyForecasts = {};
 
@@ -105,16 +118,7 @@ async function getWeatherForecast(city) {
         const hour = date.getHours();
         const temperature = forecast.main.temp;
 
-        let weatherCondition;
-        if (temperature < 5) {
-          weatherCondition = "Snow";
-        } else if (temperature >= 5 && temperature < 20) {
-          weatherCondition = "Rain";
-        } else if (temperature >= 20 && temperature < 30) {
-          weatherCondition = "Cloudy";
-        } else {
-          weatherCondition = "Sunny";
-        }
+        weatherCondition = getWeatherCondition(temperature);
 
         if (!dailyForecasts[day]) {
           dailyForecasts[day] = {
@@ -149,6 +153,71 @@ async function getWeatherForecast(city) {
     }
   } catch (error) {
     console.error("Error:", error);
-    forecastInfo.textContent = "An error occurred while fetching weather data.";
+    console.log("An error occurred while fetching weather data.");
+    alert("An error occurred while fetching weather data.");
   }
 }
+
+function capitalizeFirstLetter(text) {
+  // Split the string into words
+  const words = text.split(" ");
+
+  // Capitalize the first letter of each word
+  const capitalizedWords = words.map((word) => {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  });
+
+  // Join the capitalized words back together
+  return capitalizedWords.join(" ");
+}
+
+function getWeatherCondition(temperature) {
+  if (temperature) {
+    if (temperature < 5) {
+      return "Snow";
+    } else if (temperature >= 5 && temperature < 20) {
+      return "Rain";
+    } else if (temperature >= 20 && temperature < 30) {
+      return "Cloudy";
+    } else {
+      return "Sunny";
+    }
+  }
+}
+
+function getWeatherIcon(weatherCondition) {
+  if (weatherCondition) {
+    if (weatherCondition === "Sunny") {
+      conditionField.innerHTML = `<i class="fa-solid fa-cloud-sun"></i>`;
+    } else if (weatherCondition === "Cloudy") {
+      weatherContainer.style.backgroundImage = `url('./images/cloudy.png')`;
+      conditionField.innerHTML = `<i class="fa-solid fa-cloud"></i>`;
+    } else if (weatherCondition === "Rain") {
+      weatherContainer.style.backgroundImage = `url('./images/rain.png')`;
+      conditionField.innerHTML = `<i class="fa-solid fa-cloud-rain"></i>`;
+    } else if (weatherCondition === "Snow") {
+      weatherContainer.style.backgroundImage = `url('./images/snow.png')`;
+      conditionField.innerHTML = `<i class="fa-solid fa-snowman"></i>`;
+    } else {
+      condition.innerHTML = `<i class="fa-solid fa-cloud"></i>`;
+    }
+  } else {
+    alert("Condition not found");
+  }
+}
+
+function getTimeOfDay(time) {
+  if (time >= 4 && time <= 11) {
+    return "Morning";
+  } else if (time >= 12 && time < 16) {
+    return "Afternoon";
+  } else if (time >= 16 && time <= 21) {
+    return "Evening";
+  } else {
+    return "Night";
+  }
+}
+
+now = new Date();
+const timeOfDay = getTimeOfDay(now.getHours());
+document.getElementById("time-of-day").textContent = timeOfDay;
